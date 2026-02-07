adebeb2c6b2a1a9b1aae73ccf790ffd9e90d7b21
Florin Zamfir <f.zamfir@student.tugraz.at>
2026-02-07 14:38:08 +0100

    added missing package and enabled compilation for linux

---
+++ Music.SyncUtility/Program.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Http;
+using Music.APIs.Spotify;
+using Music.APIs.Spotify.Models;
+
+Main(null);
+
+async static void Main(string[] args)
+{ 
+  Console.WriteLine("Hello, World1!");
+  
+  // 1. get access to Spotify API
+  var services = new ServiceCollection();
+  services.AddHttpClient(); // registers IHttpClientFactory
+
+  var serviceProvider = services.BuildServiceProvider();
+  SpotifyService service = new SpotifyService(serviceProvider.GetRequiredService<IHttpClientFactory>());
+  Console.WriteLine("Spotify API initialized");
+
+  // 2. pull Spotify playlist
+  string playlistId = "0ncfcOY6o1PhQqcuwGw2W9";
+  Tracks tracks = await service.GetPlaylist(playlistId);
+
+  foreach(var track in tracks.Items)
+  {
+    Console.WriteLine(track);
+  }
+
+  // 3. convert each entry in Spotify into models
+
+  // 4. for each model, check against local database
+}
+
+Console.WriteLine("Hello, World!");
